38b7913d4b6d7d576769575efc0cc59609ce8877
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2021-03-25 13:51:36 +0500

    added movement to small grunt

--- Assets/SmallGruntMovementScript.cs
+++ Assets/SmallGruntMovementScript.cs
@@ -2,26 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementScript : MonoBehaviour
+public class SmallGruntMovementScript : MonoBehaviour
 {
     public float speed = 0.08f;
-    public float jumpForce = 7f;
-    public bool isInAir = false;
-    float xAxisInput;
     Rigidbody2D rbody;
     SpriteRenderer spriteRenderer;
     Vector3 velocityV3;
     public bool facingRight = true;
     public bool facingLeft = false;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        // transform.position += new Vector3(0, -0.8f, 0);
-        // transform.localScale.Set(2, 2, 2);
-    }
-
-    void Awake(){
         rbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -29,53 +20,59 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xAxisInput = Input.GetAxisRaw("Horizontal") * speed;
         velocityV3 = rbody.velocity;
     }
 
     void FixedUpdate(){
-        if(velocityV3.y != 0){
-            isInAir = true;
-        }
-        else{
-            isInAir = false;
-        }
+        // if(velocityV3.x == 0){
+        //     if(facingRight){
+        //         facingLeft = true;
+        //         facingRight = false;
+        //     }
+        //     else if(facingLeft){
+        //         facingRight = true;
+        //         facingLeft = false;
+        //     }
+        // }
+        // else{
+        //     move();
+        // }
+        move();
+    }
 
-        if(xAxisInput > 0){
-            xAxisInput *= Time.fixedDeltaTime;
-            moveRight();
-        }
-        if(xAxisInput < 0){
-            xAxisInput *= Time.fixedDeltaTime;
-            moveLeft();
-        }
-        if(Input.GetButton("Jump") && !isInAir){
-            jump();
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Walls"){
+            if(facingLeft){
+                facingLeft = false;
+                facingRight = true;
+                flipDirection(false);
+            }
+            else if(facingRight){
+                facingRight = false;
+                facingLeft = true;
+                flipDirection(true);
+            }
         }
     }
 
+    void move(){
+        if(facingRight)
+            moveRight();
+        else
+            moveLeft();
+    }
     void moveLeft(){
         // Debug.Log("Moved Left");
         transform.position += new Vector3(-speed, 0, 0);
         facingLeft = true;
         facingRight = false;
-        flipDirection(true);
     }
     void moveRight(){
         // Debug.Log("Moved Right");
         transform.position += new Vector3(speed, 0, 0);
         facingLeft = false;
         facingRight = true;
-        flipDirection(false);
     }
-    void jump(){
-        // Debug.Log("Jumped");
-        rbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-    }
-
-    //true means face left, false means face right
-    //flipx = false means facing right
-    //flipx = true means facing left
     void flipDirection(bool direction){
         // Debug.Log("flipped");
         if(direction)
