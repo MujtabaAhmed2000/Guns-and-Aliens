9f9e27bfc72037d5022f69c9be59f2215e314c50
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2021-03-26 01:00:39 +0500

    made bullet prefab and basic shooting

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 7f;
+    Rigidbody2D rbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.velocity = transform.right * speed;
+    }
+}
