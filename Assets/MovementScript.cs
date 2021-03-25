356c42c0e2b93a6250c1ce350eb71e0cd0fce11d
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2021-03-25 10:25:33 +0500

    level design complete

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 0.08f;
+    public float horizontalMove;
+    public float jumpForce = 0.5f;
+    bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+        if(Input.GetButtonDown("Jump")){
+            jump = true;
+        }
+    }
+
+    void FixedUpdate(){
+        // controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
