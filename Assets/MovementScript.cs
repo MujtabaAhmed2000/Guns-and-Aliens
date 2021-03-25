8b79b9324393201b12b8825245cf759396f1c722
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2021-03-25 10:55:31 +0500

    removed character controller 2D file because not working

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    public CharacterController controller;
     public float speed = 0.08f;
-    public float horizontalMove;
     public float jumpForce = 0.5f;
-    bool jump = false;
+    public float xAxisInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // transform.position += new Vector3(0, -0.8f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
-
-        if(Input.GetButtonDown("Jump")){
-            jump = true;
-        }
+        xAxisInput = Input.GetAxisRaw("Horizontal") * speed;
     }
 
     void FixedUpdate(){
-        // controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
+        if(xAxisInput > 0){
+            xAxisInput *= Time.fixedDeltaTime;
+            moveRight();
+        }
+        if(xAxisInput < 0){
+            xAxisInput *= Time.fixedDeltaTime;
+            moveLeft();
+        }
+        if(Input.GetButton("Jump")){
+            jump();
+        }
+    }
+
+    void moveLeft(){
+        Debug.Log("Moved Left");
+        transform.position += new Vector3(-speed, 0, 0);
+    }
+    void moveRight(){
+        Debug.Log("Moved Right");
+        transform.position += new Vector3(speed, 0, 0);
+    }
+    void jump(){
+        Debug.Log("Jumped");
+        // transform.position += new Vector3(0, jumpForce, 0);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
 }
