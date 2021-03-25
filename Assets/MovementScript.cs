2e4b4bd6fa57aea8234449ff216b08d7866ebcf8
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2021-03-25 12:09:41 +0500

    fix flipping

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     public float speed = 0.08f;
-    public float jumpForce = 0.5f;
-    public float xAxisInput;
+    public float jumpForce = 7f;
+    public bool isInAir = false;
+    float xAxisInput;
+    Rigidbody2D rbody;
+    Vector3 velocityV3;
+    public bool facingRight = true;
+    public bool facingLeft = false;
+    Vector3 localScale;
 
     // Start is called before the first frame update
     void Start()
     {
         // transform.position += new Vector3(0, -0.8f, 0);
+        transform.localScale.Set(2, 2, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
+        rbody = GetComponent<Rigidbody2D>();
         xAxisInput = Input.GetAxisRaw("Horizontal") * speed;
+        velocityV3 = rbody.velocity;
     }
 
     void FixedUpdate(){
+        if(velocityV3.y != 0){
+            isInAir = true;
+        }
+        else{
+            isInAir = false;
+        }
+
         if(xAxisInput > 0){
             xAxisInput *= Time.fixedDeltaTime;
             moveRight();
@@ -29,22 +45,46 @@ public class MovementScript : MonoBehaviour
             xAxisInput *= Time.fixedDeltaTime;
             moveLeft();
         }
-        if(Input.GetButton("Jump")){
+        if(Input.GetButton("Jump") && !isInAir){
             jump();
         }
     }
 
     void moveLeft(){
-        Debug.Log("Moved Left");
+        // Debug.Log("Moved Left");
         transform.position += new Vector3(-speed, 0, 0);
+        facingLeft = true;
+        facingRight = false;
+        if(facingRight){
+            flipDirection(true);
+        }
     }
     void moveRight(){
-        Debug.Log("Moved Right");
+        // Debug.Log("Moved Right");
         transform.position += new Vector3(speed, 0, 0);
+        facingLeft = false;
+        facingRight = true;
+        if(facingLeft){
+            flipDirection(false);
+        }
     }
     void jump(){
-        Debug.Log("Jumped");
-        // transform.position += new Vector3(0, jumpForce, 0);
+        // Debug.Log("Jumped");
         GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    //true means face left, false means face right
+    void flipDirection(bool direction){
+        localScale = transform.localScale;
+        // Debug.Log(transform.localScale);
+        if(direction){
+            localScale.Set(0.2f, 0.2f, 1);
+            transform.localScale = localScale;  
+        }
+        else{
+            localScale.Set(-0.2f, 0.2f, 1);
+            transform.localScale = localScale;
+        }
+        
     }
 }
