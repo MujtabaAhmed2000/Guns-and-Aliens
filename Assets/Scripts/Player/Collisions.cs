633096fd039b69b1381c91a2aa70863850e62396
infinitymon <81349188+infinitymon@users.noreply.github.com>
2021-05-06 06:57:50 +0500

    GameOverMenu is complete, score implementation still left

--- Assets/Scripts/Player/Collisions.cs
+++ Assets/Scripts/Player/Collisions.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collisions : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.tag == "Enemy"){
+    public GameObject GameOverPanel;
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Enemy") {
             Destroy(gameObject);
+            GameOver();
         }
+    }
+
+    public void GameOver()
+    {
+        GameOverPanel.SetActive(true);
+        Time.timeScale = 0.5f;
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("MainGameScene");
+        Time.timeScale = 1f;
     }
 }
